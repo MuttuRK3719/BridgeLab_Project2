96ec92670610bff6094fa14aa18d8244a028d6a0
Muttu <kagalmutturaj@gmail.com>
2025-08-19 11:53:03 +0530

    After creating add feature to class

---
+++ src/addressbook/AddPerson.java
@@ -0,0 +1,27 @@
+package addressbook;
+
+import java.util.Scanner;
+
+public class AddPerson {
+    Scanner scan = new Scanner(System.in);
+
+    public void addContact() {
+        Contact person = new Contact();
+        System.out.println("Enter person First Name: ");
+        person.setFirstName(scan.next());
+        System.out.println("Enter person Last Name: ");
+        person.setLastName(scan.next());
+        System.out.println("Enter person city : ");
+        person.setCity(scan.next());
+        System.out.println("Enter person state Name: ");
+        person.setState(scan.next());
+        System.out.println("Enter person Address Name: ");
+        person.setAddress(scan.next());
+        System.out.println("Enter person Zip code: ");
+        person.setZipCode(scan.nextInt());
+        System.out.println("Enter person Phone number : ");
+        person.setPhoneNumber(scan.nextLong());
+        System.out.println("Enter person email : ");
+        person.setEmail(scan.next());
+    }
+}
