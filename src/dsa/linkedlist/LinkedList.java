b2fe2f7693772132df4f0569186bec26cb3dc405
Muttu <kagalmutturaj@gmail.com>
2025-08-19 15:45:17 +0530

    Implimenting single Linked List

---
+++ src/dsa/linkedlist/LinkedList.java
@@ -0,0 +1,49 @@
+package dsa.linkedlist;
+
+public class LinkedList {
+    Node head;
+    Node tail;
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(10);
+        ll.add(20);
+        ll.add(30);
+        ll.add(40);
+        ll.add(50);
+        ll.add(60);
+        ll.display();
+        ReverseLinkedList list = new ReverseLinkedList();
+        Node node = list.reverseLinkedList(ll.head);
+        ll.display(node);
+    }
+
+    void add(int val) {
+        if (head == null) {
+            head = new Node(val);
+            tail = head;
+            return;
+        }
+        Node n = new Node(val);
+        tail.next = n;
+        tail = n;
+    }
+
+    void display(Node temp) {
+        while (temp != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println("END");
+
+    }
+}
---
+++ src/dsa/linkedlist/Node.java
@@ -0,0 +1,10 @@
+package dsa.linkedlist;
+
+public class Node {
+    int value;
+    Node next;
+
+    Node(int val) {
+        this.value = val;
+    }
+}
