f54b1fcc07acdd9f42f25f57a1e5bb8e686e592c
Muttu <kagalmutturaj@gmail.com>
2025-08-19 15:25:05 +0530

    After adding bookList and implementing searching by using bookList

--- src/addressbook/AddressBook.java
+++ src/addressbook/AddressBook.java
@@ -1,15 +1,22 @@
 package addressbook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AddressBook {
+    private static Map<String, List<Contact>> addressBooks = new HashMap<>();
+
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
         List<Contact> contactList = new ArrayList<>();
-        ModifyPersonClassInfo personClassInfo=new ModifyPersonClassInfo();
+
+        ModifyPersonClassInfo personClassInfo = new ModifyPersonClassInfo();
         AddPerson addPerson = new AddPerson();
         Contact person = addPerson.addContact();
         contactList.add(person);
+        addressBooks.put("Book1", contactList);
+        personClassInfo.updatePerson(addressBooks);
     }
 }
--- src/addressbook/ModifyPersonClassInfo.java
+++ src/addressbook/ModifyPersonClassInfo.java
@@ -1,50 +1,67 @@
 package addressbook;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ModifyPersonClassInfo {
     Scanner scan = new Scanner(System.in);
 
-    public boolean updatePerson(List<Contact> contactList) {
-        System.out.println("Enter person Last Name: ");
+    public boolean updatePerson(Map<String, List<Contact>> addressBooks) {
+        System.out.println("Enter person First Name to update: ");
         String firstName = scan.next();
-        int index = 0;
-        for (Contact contact : contactList) {
-            if (contact.getFirstName().equals(firstName)) {
-                break;
-            }
-            index++;
-        }
-        if (index == contactList.size()) {
-            System.out.println("This type first name of person is not available in book");
-            return false;
-        }
-        Contact person = new Contact();
-        System.out.println("Enter person Last Name: ");
-        person.setLastName(scan.next());
-        System.out.println("Enter person city : ");
-        person.setCity(scan.next());
-        System.out.println("Enter person state Name: ");
-        person.setState(scan.next());
-        System.out.println("Enter person Address Name: ");
-        person.setAddress(scan.next());
-        System.out.println("Enter person Zip code: ");
-        person.setZipCode(scan.nextInt());
-        System.out.println("Enter person Phone number : ");
-        person.setPhoneNumber(scan.nextLong());
-        System.out.println("Enter person email : ");
-        person.setEmail(scan.next());
-        contactList.set(index,person);
-        return true;
-    }
+        boolean found = false;
+        for (Map.Entry<String, List<Contact>> entry : addressBooks.entrySet()) {
+            List<Contact> contactList = entry.getValue();
+            for (int i = 0; i < contactList.size(); i++) {
+                Contact contact = contactList.get(i);
+                if (contact.getFirstName().equalsIgnoreCase(firstName)) {
+                    found = true;
 
-    public boolean deletePerson(String firstName) {
-        System.out.println("No such person not available with name of : " + firstName);
+                    Contact updatedContact = new Contact();
+                    System.out.println("Enter new Last Name: ");
+                    updatedContact.setLastName(scan.next());
+                    System.out.println("Enter new City: ");
+                    updatedContact.setCity(scan.next());
+                    System.out.println("Enter new State: ");
+                    updatedContact.setState(scan.next());
+                    System.out.println("Enter new Address: ");
+                    updatedContact.setAddress(scan.next());
+                    System.out.println("Enter new Zip Code: ");
+                    updatedContact.setZipCode(scan.nextInt());
+                    System.out.println("Enter new Phone Number: ");
+                    updatedContact.setPhoneNumber(scan.nextLong());
+                    System.out.println("Enter new Email: ");
+                    updatedContact.setEmail(scan.next());
+
+                    // Preserve original first name
+                    updatedContact.setFirstName(contact.getFirstName());
+
+                    contactList.set(i, updatedContact);
+                    System.out.println("Contact updated successfully.");
+
+                }
+
+            }
+        }
+        if (!found) {
+            System.out.println("No contact found with the given first name.");
+        }
         return false;
     }
-    public boolean deletePerson(String firstName) {
-        System.out.println("No such person not available with name of : " + firstName);
+
+    public boolean deletePerson(Map<String, List<Contact>> addressBooks, String firstName) {
+        for (Map.Entry<String, List<Contact>> entry : addressBooks.entrySet()) {
+            List<Contact> contactList = entry.getValue();
+            for (int i = 0; i < contactList.size(); i++) {
+                if (contactList.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+                    contactList.remove(i);
+                    System.out.println("Contact deleted successfully.");
+                    return true;
+                }
+            }
+        }
+        System.out.println("No contact found with the name: " + firstName);
         return false;
     }
 }
