a071af779a749ee52cf8567d9c83816473f25b30
Muttu <kagalmutturaj@gmail.com>
2025-08-19 12:00:32 +0530

    adding update feature to Address book

---
+++ src/addressbook/ModifyPersonClassInfo.java
@@ -0,0 +1,28 @@
+package addressbook;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ModifyPersonClassInfo {
+    Scanner scan = new Scanner(System.in);
+
+    public boolean updatePerson(String firstName) {
+        Contact person = new Contact();
+        System.out.println("Enter person Last Name: ");
+        person.setLastName(scan.next());
+        System.out.println("Enter person city : ");
+        person.setCity(scan.next());
+        System.out.println("Enter person state Name: ");
+        person.setState(scan.next());
+        System.out.println("Enter person Address Name: ");
+        person.setAddress(scan.next());
+        System.out.println("Enter person Zip code: ");
+        person.setZipCode(scan.nextInt());
+        System.out.println("Enter person Phone number : ");
+        person.setPhoneNumber(scan.nextLong());
+        System.out.println("Enter person email : ");
+        person.setEmail(scan.next());
+
+        return false;
+    }
+}
