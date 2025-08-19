c5ec0a32b8c3dacdf728972445f8ee5030ddb700
Muttu <kagalmutturaj@gmail.com>
2025-08-19 12:13:49 +0530

    After adding multiple person feature to addres book

--- src/addressbook/AddressBook.java
+++ src/addressbook/AddressBook.java
@@ -1,7 +1,15 @@
 package addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+        List<Contact> contactList = new ArrayList<>();
+        ModifyPersonClassInfo personClassInfo=new ModifyPersonClassInfo();
+        AddPerson addPerson=new AddPerson();
+        Contact person=addPerson.addContact();
+        contactList.add(person);
     }
 }
