e404826fe15caaace56ae5b9ad853d653f1a26f3
Muttu <kagalmutturaj@gmail.com>
2025-08-19 12:20:44 +0530

    After creating list of multiple person

--- src/addressbook/AddressBook.java
+++ src/addressbook/AddressBook.java
@@ -1,7 +1,14 @@
 package addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+        List<Contact> contactList = new ArrayList<>();
+        AddPerson addPerson = new AddPerson();
+        Contact person = addPerson.addContact();
+        contactList.add(person);
     }
 }
