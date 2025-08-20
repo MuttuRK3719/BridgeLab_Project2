207b3496fa4741550a007f952f1593c576cee7f3
Muttu <kagalmutturaj@gmail.com>
2025-08-20 12:47:39 +0530

    after implementing class of NextGreater element

--- src/dsa/stack/Stack.java
+++ src/dsa/stack/Stack.java
@@ -1,4 +1,51 @@
 package dsa.stack;
 
-public class Stack {
+public class Stack<T> {
+    int size = 10;
+    T[] stackArray;
+    int index = 0;
+
+    Stack() {
+        stackArray = (T[]) new Object[size];
+    }
+
+    void push(T ch) {
+//        if (index > stackArray.length) {
+//            char[] temp = new char[2 * stackArray.length];
+//            for (int i = 0; i < stackArray.length; i++) {
+//                temp[i] = stackArray[i];
+//                size=stackArray.length;
+//            }
+//            stackArray = temp;
+//        }
+        stackArray[index++] = ch;
+    }
+
+    T pop() {
+        if (index >= 0) {
+            T ch = stackArray[index];
+            index--;
+            return ch;
+        } else {
+            throw new IndexOutOfBoundsException();
+
+        }
+    }
+
+    boolean isEmpty() {
+        return index == 0;
+    }
+
+    T peek() {
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException();
+        }
+        return stackArray[index-1];
+    }
+
+    int size() {
+        return index;
+    }
+
+
 }
