ccbcccf59deead12fb2d524cb4bf6f1e6b6d067e
Muttu <kagalmutturaj@gmail.com>
2025-08-21 16:15:41 +0530

    Implemented reversing elements in queue

---
+++ src/dsa/queue1/ReverseElements.java
@@ -0,0 +1,26 @@
+package dsa.queue1;
+
+import java.util.Queue;
+
+public class ReverseElements {
+    public static void main(String[] args) {
+        QueueUsingStack<Integer> que = new QueueUsingStack<>();
+        que.offer(10);
+        que.offer(20);
+        que.offer(30);
+        que.offer(40);
+        que.offer(50);
+        reverseQueue(que);
+        while (!que.isEmpty()) {
+            System.out.println(que.poll());
+        }
+
+    }
+
+    static void reverseQueue(QueueUsingStack<Integer> queue) {
+        if (queue.isEmpty()) return;
+        int element = queue.poll();
+        reverseQueue(queue);
+        queue.offer(element);
+    }
+}
