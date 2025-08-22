7bd603d1a7a31fead7c1bf7868cf22d0b496d357
Muttu <kagalmutturaj@gmail.com>
2025-08-22 12:58:29 +0530

    implementing class for printing first non repeating element

---
+++ src/collections/FirstNonDublicate.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FirstNonDublicate {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(4, 5, 6, 4, 7, 8, 5);
+        Set<Integer> values = linkedSetInsertion(list);
+        System.out.println(values.iterator().next());
+    }
+
+    static Set<Integer> linkedSetInsertion(List<Integer> list) {
+        Set<Integer> linkedHashset = new LinkedHashSet<>();
+        for (int element : list) {
+            if (linkedHashset.contains(element)) {
+                linkedHashset.remove(element);
+            } else {
+
+                linkedHashset.add(element);
+            }
+        }
+        return linkedHashset;
+    }
+}
