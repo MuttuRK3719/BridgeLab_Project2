72497c206d1f224a095d871ac263ced997de19ac
Muttu <kagalmutturaj@gmail.com>
2025-08-25 19:53:10 +0530

    Implementing class for to list anagram using map

--- src/customcollections/map/AnagramList.java
+++ src/customcollections/map/AnagramList.java
@@ -4,17 +4,56 @@ import java.util.*;
 
 public class AnagramList {
     public static void main(String[] args) {
-    List<String>list=List.of("eat", "tea", "tan", "ate", "nat", "bat");
-        System.out.println(listAnagrams(list));
+        List<String> list = List.of("eat", "tea", "tan", "ate", "nat", "bat");
+        System.out.println(listAnagrams3(list));
     }
-    public static List<List<String>> listAnagrams(List<String>list){
-        Map<String,List<String>>freq=new HashMap<>();
-        for(String s:list){
-            char []chs=s.toCharArray();
+
+    public static List<List<String>> listAnagrams(List<String> list) {
+        Map<String, List<String>> freq = new HashMap<>();
+        for (String s : list) {
+            char[] chs = s.toCharArray();
             Arrays.sort(chs);
-            String sortedstr=new String(chs);
-            freq.computeIfAbsent(sortedstr,k->new ArrayList<>()).add(s);
+            String sortedstr = new String(chs);
+            freq.computeIfAbsent(sortedstr, k -> new ArrayList<>()).add(s);
         }
         return new ArrayList<>(freq.values());
+    }
+
+    public static List<List<String>> listAnagrams2(List<String> list) {
+        Map<String, List<String>> freq = new HashMap<>();
+        for (String s : list) {
+            char[] chs = s.toCharArray();
+            Arrays.sort(chs);
+            String sortedstr = new String(chs);
+            freq.compute(sortedstr, (k, v) -> {
+                if (v == null) {
+                    return new ArrayList<>(List.of(s));
+                } else {
+                    v.add(s);
+                    return v;
+                }
+            });
+        }
+        return new ArrayList<>(freq.values());
+    }
+
+    public static List<List<String>> listAnagrams3(List<String> list) {
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for (String word : list) {
+            String sorted = word.chars()
+                    .sorted()
+                    .collect(StringBuilder::new,
+                            StringBuilder::appendCodePoint,
+                            StringBuilder::append)
+                    .toString();
+
+            groups.merge(sorted, new ArrayList<>(List.of(word)),
+                    (oldList, newList) -> {
+                        oldList.addAll(newList);
+                        return oldList;
+                    });
+        }
+        return new ArrayList<>(groups.values());
     }
 }
--- src/customcollections/map/FrequencyCounter.java
+++ src/customcollections/map/FrequencyCounter.java
@@ -6,13 +6,37 @@ import java.util.Map;
 
 public class FrequencyCounter {
     public static void main(String[] args) {
-        countFrequency("mutturaj");
+        countFrequency2("mutturaj");
     }
 
     static void countFrequency(String word) {
         Map<Character, Integer> freq = new HashMap<>();
         for (int i = 0; i < word.length(); i++) {
             freq.put(word.charAt(i), freq.getOrDefault(word.charAt(i), 0) + 1);
+        }
+        for (var map : freq.entrySet()) {
+            System.out.println(map.getKey() + " " + map.getValue());
+        }
+    }
+
+    static void countFrequency1(String word) {
+        Map<Character, Integer> freq = new LinkedHashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            //if it is present
+            freq.computeIfPresent(ch, (k, v) -> v + 1);
+            //if it is not present
+            freq.putIfAbsent(ch, 1);
+        }
+        for (var map : freq.entrySet()) {
+            System.out.println(map.getKey() + " " + map.getValue());
+        }
+    }
+    static void countFrequency2(String word) {
+        Map<Character, Integer> freq = new LinkedHashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            freq.compute(ch,(k,v)->v==null?1:v+1);
         }
         for (var map : freq.entrySet()) {
             System.out.println(map.getKey() + " " + map.getValue());
