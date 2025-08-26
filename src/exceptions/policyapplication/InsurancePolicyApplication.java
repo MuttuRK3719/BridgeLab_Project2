ef6dabd9a6ce845360548aeeec3c3568946d066a
Muttu <kagalmutturaj@gmail.com>
2025-08-26 15:42:32 +0530

    updating  insurance policy project

--- src/exceptions/policyapplication/InsurancePolicyApplication.java
+++ src/exceptions/policyapplication/InsurancePolicyApplication.java
@@ -1,7 +1,9 @@
 package exceptions.policyapplication;
 
+import exceptions.customexception.unchecked.ExceptionChaining;
+
 public class InsurancePolicyApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidAgeException, HealthIssueException, PoorDrivingRecordException {
         Person person = new Person("Muttu", 23, 10, 10);
         Person person2 = new Person("xyz", 25, 3, 7);
         Person person3 = new Person("abc", 17, 9, 7);
@@ -11,20 +13,12 @@ public class InsurancePolicyApplication {
 
     }
 
-    static void verification(Person person) {
-        try {
+    static void verification(Person person) throws InvalidAgeException,HealthIssueException,PoorDrivingRecordException {
+
             if (person.getAge() < 18) throw new InvalidAgeException();
             else if (person.getHealthRate() < 5) throw new HealthIssueException();
             else if (person.getDrivingRate() < 7) throw new PoorDrivingRecordException();
             else System.out.println("U are eligible for all the insurances");
-        } catch (InvalidAgeException exception) {
-            System.out.println(exception.getMessage());
-        } catch (HealthIssueException exception) {
-            System.out.println(exception.getMessage());
-        } catch (PoorDrivingRecordException exception) {
-            System.out.println(exception.getMessage());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+
     }
 }
