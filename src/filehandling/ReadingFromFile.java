5ef65299b1921d7d52f9b8a856460b5a1b3c2ef2
Muttu <kagalmutturaj@gmail.com>
2025-09-02 12:24:49 +0530

    Designed class to read from file  until it empty

---
+++ src/filehandling/ReadingFromFile.java
@@ -0,0 +1,14 @@
+package filehandling;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadingFromFile {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("WritingFile.txt");
+        int data = 0;
+        while ((data = fileInputStream.read()) != -1) {
+            System.out.println((char) (data));
+        }
+    }
+}
