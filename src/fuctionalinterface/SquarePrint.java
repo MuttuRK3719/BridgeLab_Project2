8698081b4ab58586a6ce1c372064e9ce50695376
Muttu <kagalmutturaj@gmail.com>
2025-09-09 15:59:28 +0530

    calculate square of num

---
+++ src/fuctionalinterface/SquarePrint.java
@@ -0,0 +1,16 @@
+package fuctionalinterface;
+
+public class SquarePrint {
+    public static void main(String[] args) {
+        CustomInterface customInterface = x -> x * x;
+        customInterface.print(2);
+    }
+}
+
+interface CustomInterface {
+    int square(int n);
+
+    default void print(int n) {
+        System.out.println("The square of number is " + square(n));
+    }
+}
