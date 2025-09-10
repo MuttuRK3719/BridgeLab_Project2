cf6eb4a7c4126725dabf7af3581dd916e6bbab2f
Muttu <kagalmutturaj@gmail.com>
2025-09-10 22:06:59 +0530

    After printing list using foreach method

--- src/java8features/stream/TerminalOperators.java
+++ src/java8features/stream/TerminalOperators.java
@@ -1,5 +1,13 @@
 package java8features.stream;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class TerminalOperators {
+    //    Print all elements of a list of strings.
+    static void printUsingForEach(List<Integer> arr) {
+        arr.forEach(System.out::println);
+    }
 
 }
