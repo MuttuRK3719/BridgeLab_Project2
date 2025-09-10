b036810eaa4df0e0719d064d35d46c4b9bf9ae1b
Muttu <kagalmutturaj@gmail.com>
2025-09-10 22:01:07 +0530

    After implemeting logic for return list of string length

--- src/java8features/stream/Intermediate.java
+++ src/java8features/stream/Intermediate.java
@@ -10,5 +10,8 @@ public class Intermediate {
     static List<Integer> filter(List<Integer> arr) {
         return arr.stream().filter(i -> i > 50).toList();
     }
-
+    //    list of strings, return a list of their lengths.
+    static List<Integer> lengthOfString(List<String> arr) {
+        return arr.stream().map(String::length).collect(Collectors.toList());
+    }
 }
