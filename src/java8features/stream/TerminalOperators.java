76d56a5dcb5be8c10a816bcfdb96568cfced6cb2
Muttu <kagalmutturaj@gmail.com>
2025-09-10 22:14:46 +0530

    counting names starts with A

--- src/java8features/stream/TerminalOperators.java
+++ src/java8features/stream/TerminalOperators.java
@@ -9,12 +9,19 @@ public class TerminalOperators {
     static void printUsingForEach(List<Integer> arr) {
         arr.forEach(System.out::println);
     }
+
     //    Convert a list of integers into a set.
     static Set<Integer> listToSet(List<Integer> arr) {
         return arr.stream().collect(Collectors.toSet());
     }
+
     //    product of all numbers in a list.
     static long reduceToProduct(List<Integer> arr) {
         return arr.stream().reduce(1, (a, b) -> a * b);
+    }
+
+//    counting the name starts with A
+    static long countString(List<String> names) {
+        return names.stream().filter(name -> name.startsWith("A")).count();
     }
 }
