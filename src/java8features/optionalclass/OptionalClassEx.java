01f2523d20acf6fb35d83d378bf8882c07a913ec
Muttu <kagalmutturaj@gmail.com>
2025-09-11 11:35:33 +0530

    implementing logic to get value or return default value

--- src/java8features/optionalclass/OptionalClassEx.java
+++ src/java8features/optionalclass/OptionalClassEx.java
@@ -7,11 +7,20 @@ public class OptionalClassEx {
         String st = "MUTTU";
         System.out.println(getName(st).orElse("Default"));
         System.out.println(getName(null));
+        String email=null;
+        String email2="muttu@gmail.com";
+        System.out.println(orElseNull(email));
+        System.out.println(orElseNull(email2));
 
     }
 
     //    returns an Optional<String> for a username.
     public static Optional<String> getName(String name) {
         return Optional.ofNullable(name);
+    }
+
+    //    default value if email is null.
+    public static String orElseNull(String name) {
+        return Optional.ofNullable(name).orElse("null@gmail.com");
     }
 }
