5c6ecefe14008d068e8fab69eeea20989f0cdf24
Muttu <kagalmutturaj@gmail.com>
2025-09-25 16:40:18 +0530

    Comparing flexibility between Thread and Runnalbe

---
+++ src/multithreading/fooddelivery/ChecFlexibility.java
@@ -0,0 +1,17 @@
+package multithreading.fooddelivery;
+
+public class ChecFlexibility {
+    public static void main(String[] args) {
+        Thread restarent=new Thread(()->{
+            System.out.println("Extending the thread class");
+        });
+        Runnable deliveryAgent=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Implementing Runnable interface");
+            }
+        };
+        restarent.start();
+        new Thread(deliveryAgent).start();
+    }
+}
