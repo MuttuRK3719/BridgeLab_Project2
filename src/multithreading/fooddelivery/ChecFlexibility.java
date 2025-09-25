d7de8dacb4f37d3832d9edb130a53577e0a6b1fb
Muttu <kagalmutturaj@gmail.com>
2025-09-25 16:43:25 +0530

    Comparing flexibility between threadclass and Runnable interface

--- src/multithreading/fooddelivery/ChecFlexibility.java
+++ src/multithreading/fooddelivery/ChecFlexibility.java
@@ -2,10 +2,10 @@ package multithreading.fooddelivery;
 
 public class ChecFlexibility {
     public static void main(String[] args) {
-        Thread restarent=new Thread(()->{
+        Thread restarent = new Thread(() -> {
             System.out.println("Extending the thread class");
         });
-        Runnable deliveryAgent=new Runnable() {
+        Runnable deliveryAgent = new Runnable() {
             @Override
             public void run() {
                 System.out.println("Implementing Runnable interface");
